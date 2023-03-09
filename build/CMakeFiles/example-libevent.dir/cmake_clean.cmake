f53010e40ab268d6973a57da81d6443fdd0960d0
bee06 <zong.jianhui06@gmail.com>
2023-03-09 09:44:04 +0800

    compile

---
+++ build/CMakeFiles/example-libevent.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example-libevent.dir/example-libevent.o"
+  "CMakeFiles/example-libevent.dir/example-libevent.o.d"
+  "example-libevent"
+  "example-libevent.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/example-libevent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/example-libevent.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for example-libevent.
+# This may be replaced when dependencies are built.
