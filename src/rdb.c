37eb6e9294284e61ccd4b737efb5a2ccf6eb3b4f
bee06 <zong.jianhui06@gmail.com>
2023-03-15 18:15:02 +0800

    更新注释

--- src/rdb.c
+++ src/rdb.c
@@ -27,6 +27,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
 #include "server.h"
 #include "lzf.h"    /* LZF compression library */
 #include "zipmap.h"
